3412c36dc6de6948c3d937fb1ecfcc358ab49cf2
Foskary <luis_gustavo_20@hotmail.es>
2020-06-05 16:54:47 +0200

    migLayout

---
+++ Proyecto_Biblioteca/src/vista/Prueba.java
@@ -0,0 +1,29 @@
+package vista;
+
+import java.awt.Component;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Prueba extends JFrame implements ActionListener, WindowListener {
+	
+	private JLabel id;
+	private JTextField escribirId;
+	private JButton acceso;
+	private JPanel panel;
+	
+	public Prueba() {
+		crearVentana();
+	}
+	
+	public void crearVentana() {
+		
+		panel = new JPanel();
+		panel.setLayout(new );
+	}
+}
