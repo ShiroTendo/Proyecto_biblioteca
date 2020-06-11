d0aa4f85bf9a60cc3d6799657e02c591a2a8216e
ShiroTendo <sergiofernandez.cue@gmail.com>
2020-06-11 10:37:51 +0200

    Commit_Cambios_Pruebas

---
+++ Proyecto_Biblioteca/src/pruebas/Test_Buscadores.java
@@ -0,0 +1,52 @@
+package pruebas;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import modelo.Biblioteca;
+import modelo.Libros;
+import modelo.Socio;
+
+public class Test_Buscadores {
+	
+
+	@Test
+	public void buscarSocioNull() throws ClassNotFoundException, SQLException {
+		Biblioteca aux= new Biblioteca();
+		assertNull(aux.buscarSocio(20));
+	}
+	@Test
+	public void buscarSocioNotNull() throws ClassNotFoundException, SQLException {
+		Biblioteca aux= new Biblioteca();
+		assertNotNull(aux.buscarSocio(1));
+	}
+	
+	@Test
+	public void buscarLibroNotNull() throws ClassNotFoundException, SQLException {
+		Biblioteca aux= new Biblioteca();
+		assertNull(aux.buscaLibro(20));
+	}
+	@Test
+	public void buscarLibroNull() throws ClassNotFoundException, SQLException {
+		Biblioteca aux= new Biblioteca();
+		assertNotNull(aux.buscaLibro(1));
+	}
+	
+	@Test
+	public void buscarBibliotecarioNotNull() throws ClassNotFoundException, SQLException {
+		Biblioteca aux= new Biblioteca();
+		assertNull(aux.buscarBibliotecario(20));
+	}
+	@Test
+	public void buscarBibliotecarioNull() throws ClassNotFoundException, SQLException {
+		Biblioteca aux= new Biblioteca();
+		assertNotNull(aux.buscarBibliotecario(1));
+	}
+	
+
+
+
+}
